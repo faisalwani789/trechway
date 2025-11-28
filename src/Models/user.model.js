ebfd510ed35c434f58631067aba294f3c25f29f4
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-11-28 22:13:52 +0530

    added userModel

---
+++ src/Models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: {
+        type: 'String',
+        minLength: 4,
+    
+    },
+    email:{
+        type:'String',
+        required:true
+    },
+    userName: {
+        type: 'String',
+        minLength: 5
+    },
+    password: {
+        type: 'String',
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(value);
+            },
+            message:
+                "Password must be at least 8 characters and include upper & lowercase letters, a number, and a special character."
+        }
+    }
+})
+export default mongoose.model=mongoose.model("User",userSchema)
