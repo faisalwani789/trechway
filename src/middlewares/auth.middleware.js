2f8355726d2d66743962b09150769405fca3610b
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-11-29 12:19:44 +0530

    added auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    console.log(authHeader)
+    if (!authHeader || !authHeader.startsWith('Bearer')) return res.status(403).send("resourse not allowed")
+    const token = authHeader.split(" ")[1]
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+
+}
