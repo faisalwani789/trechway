7d0800caccc12dd0232b073b3d9545f89c73df41
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-12-05 22:08:39 +0530

    added user controller

---
+++ src/Controllers/user.controller.js
@@ -0,0 +1,24 @@
+import ConnectionRequest from "../Models/connection.request.model.js"
+
+export const ViewConnections=async(req,res)=>{
+    const{id}=req.user
+    try {
+        const requests= await ConnectionRequest.find({$or:[
+            {toUserId:id,status:'accepted'},//send by others to the logged in user
+            {fromUserId:id,status:'accepted'}//send by the logged in user
+        ]}).populate('toUserId','name profilePic').populate('fromUserId','name profilePic') // this will send populated version of both sender and receiver but in real we need data of other member not logged in, 
+        
+        const filteredRequest=requests.map(req=>{
+            if(req.fromUserId.equals(id)){
+                return req.toUserId
+            }
+            else{
+                return req.fromUserId
+            }
+        })
+        
+        res.json(filteredRequest)
+    } catch (error) {
+         res.status(500).json({success:false,message:error.message})
+    }
+}
