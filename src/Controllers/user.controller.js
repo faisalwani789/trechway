3a297ce69baeca3f16ca4e1889f451d56dac774c
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-12-06 21:55:26 +0530

    added user recommendations + cursor pagination

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,4 +1,5 @@
 import ConnectionRequest from "../Models/connection.request.model.js"
+import User from '../Models/user.model.js'
 export const ViewRequests=async(req,res)=>{
     const{id}=req.user
     try {
@@ -29,5 +30,47 @@ export const ViewConnections=async(req,res)=>{
         res.status(200).json(filteredRequest)
     } catch (error) {
          res.status(500).json({success:false,message:error.message})
+    }
+}
+
+export const RecommendedConnections=async(req,res)=>{
+    const{cursor,limit}=req.query
+    const{id}=req.user
+    const query={}
+    const conns=new Set()
+    let nextCursor=null
+    try {
+        if(cursor){
+            query._id={$lt:cursor}
+        }
+        const UserConnections=await ConnectionRequest.find({$or:[
+            {fromUserId:id},
+            {toUserId:id}
+        ]}).select('fromUserId toUserId')
+
+        UserConnections.forEach(con=>{
+            conns.add(con.fromUserId);
+            conns.add(con.toUserId)
+        })
+
+        let recommendations=await User.find({
+            $and:[
+                { _id:{ $nin:Array.from(conns)}},
+                {_id: {$ne:id}}
+            ],
+            ...query    
+        }).sort({_id:-1}).limit(parseInt(limit)+1).select('userName _id profilePic')
+        
+        console.log(recommendations.length +limit)
+        if(recommendations.length > limit){
+            
+            nextCursor=recommendations[limit]._id
+            recommendations=recommendations.slice(0,limit)
+        }
+        res.status(200).json({recommendations,nextCursor})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success:false,message:error.message})
+        
     }
 }
--- src/router/user.routes.js
+++ src/router/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { ViewConnections,ViewRequests } from "../Controllers/user.controller.js";
+import { ViewConnections,ViewRequests,RecommendedConnections } from "../Controllers/user.controller.js";
 const router=Router()
 router.get("/connection/requests",ViewRequests)//received requests only 
 router.get("/connections",ViewConnections)
+router.get('/connection/recomendations',RecommendedConnections)
 
 
 export default router
