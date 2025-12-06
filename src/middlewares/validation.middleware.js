2b892239d6b9cb5e909d4431324b080ac535bc2f
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-12-06 22:34:39 +0530

    added post validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,4 +1,4 @@
-import { validationResult ,body} from "express-validator";
+import { validationResult, body } from "express-validator";
 
 export const registerValidator = [
   body('email')
@@ -21,13 +21,18 @@ export const loginValidator = [
     .isLength({ min: 8 })
     .withMessage('Password must be at least 6 characters'),
 ];
-export const passwordValidator=[
+export const passwordValidator = [
   body('password')
     .notEmpty().withMessage('Password is required')
     .isLength({ min: 8 }).withMessage('Password must be at least 6 characters')
     .isStrongPassword(),
-  body('confirmPassword').custom((value,{req})=>{
-        if(value!==req.body.password) throw new Error("passwords do not match")
-          return true
-      })
+  body('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) throw new Error("passwords do not match")
+    return true
+  })
+]
+
+export const postValidator = [
+  body('description').notEmpty().withMessage('description is empty').isLength({ max: 100 }).withMessage("description can't be more than 100 characters"),
+  body('tags').isLength({ max: 100 }).withMessage('tags can not be more than 100 characters')
 ]
--- src/router/feed.routes.js
+++ src/router/feed.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getFeed } from "../Controllers/feed.controller";
+import { getFeed } from "../Controllers/feed.controller.js";
+import { postValidator } from "../middlewares/validation.middleware.js";
 const router=Router()
 
-router.get('/',getFeed)
+router.get('/',postValidator,getFeed)
 
 export default router
